8544deeabec6c8335e371d3aa150f90a4c898642
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2023-09-05 15:14:12 -0300

    feat: adding antd samples

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,38 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.less';
+import { Space, DatePicker, ConfigProvider, version } from 'antd';
 
+import Button from '../components/button';
 import NxWelcome from './nx-welcome';
+import styles from './app.module.less';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="act-components" />
-    </div>
+    <>
+      <div className={styles['act-components-antd-example']}>
+        <h1>antd version: {version}</h1>
+        <ConfigProvider
+          theme={{
+            token: {
+              // Seed Token
+              colorPrimary: '#00b96b',
+              borderRadius: 2,
+
+              // Alias Token
+              colorBgContainer: '#f6ffed',
+            },
+          }}
+        >
+          <Space>
+            <DatePicker />
+            <Button type="primary" ghost={false}>
+              Primary Button
+            </Button>
+          </Space>
+        </ConfigProvider>
+      </div>
+      <div>
+        <NxWelcome title="act-components" />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/button/index.tsx
@@ -0,0 +1,5 @@
+import { Button, ButtonProps } from "antd";
+
+export default (props: ButtonProps) => {
+  return <Button ghost {...props} />;
+}
